63c969da9b7ab68b959f53f0ff7b914ea603c9ac
Prabhsimran Singh <prabhsimransingh24092@gmail.com>
2023-04-27 00:13:19 +0530

    fixed projects model

---
+++ portfolio/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+class Contact(models.Model):
+    name= models.CharField(max_length=25)
+    email= models.EmailField()
+    phonenumber= models.CharField(max_length=10)
+    description= models.TextField()
+
+    def __str__(self):
+        return self.name
+
+class Projects(models.Model):
+    title=models.CharField(max_length=60)
+    description=models.TextField()
+    img=models.ImageField(upload_to='projects', blank=True, null=True)
+    link=models.URLField( max_length=128)
+
+    def __str__(self):
+        return self.title
+
+
